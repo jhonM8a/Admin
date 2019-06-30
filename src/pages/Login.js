34109b18338b3a3d28371df1ae51063900e514a7
jhonochoa <jhon.ochoa@unillanos.edu.co>
2019-06-30 11:49:01 -0500

    Se termina el login Se hace uso de icons en input. Se agrega footer de copyrigth. Se cambia nombres de input y agrega nombre 'Admin Module'.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import LoginForm from "../components/LoginForm";
+import Footer from "../components/FooterCopyRight";
 
 class Login extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class Login extends React.Component {
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
